f4a71c7f3e6614d0be87f009499c9f56e180050c
wangzhaohan2910 <wangcf3000@sina.com>
2025-07-28 15:07:47 +0800

    0

---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+AUTHOR = 'wangzhaohan2910'
+SITENAME = 'wangzhaohan2910的博客'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Asia/Shanghai'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Local site", "#")
+)
+
+# Social widget
+SOCIAL = (
+    ("Github tells everything", "https://github.com/wangzhaohan2910"),
+)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
